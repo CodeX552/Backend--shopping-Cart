58e3ca75def0e9230ba925ecb6b918ed7f91ddcd
Tarun Kumar <146514355+codebyTarun08@users.noreply.github.com>
2025-11-23 15:25:08 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://backend-shopping-cart-8jog.onrender.com",
 });
 
 export function setAuthToken(token) {
